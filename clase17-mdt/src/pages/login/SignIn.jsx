d0276fb4981a993d97f3d5935926cad8b4359339
Rociojc <ennarociojc@gmail.com>
2022-06-30 11:49:04 -0500

    Se usa formik

--- clase17-mdt/src/pages/login/SignIn.jsx
+++ clase17-mdt/src/pages/login/SignIn.jsx
@@ -1,28 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import GoTo from '../../components/GoTo';
 import { Boton, Formulario, Input, Seccion, Text2, Titulo } from '../../style/LoginStyle';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useFormik } from 'formik';
 
 function SignIn({ setIsLogged }) {
-    const navigate = useNavigate();
-    const handlerSubmit = (e) => {
-    e.preventDefault();
-    setIsLogged(true);
-    navigate("/home");
-  }
+  const [showPassword, setShowPassword] = useState(false)
+
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    onSubmit: (values) => {
+      setIsLogged(true);
+      navigate("/home");
+    }
+  })
+
   return (
     <>
-    <Formulario onSubmit={handlerSubmit}>
+    <Formulario onSubmit={formik.handleSubmit}>
       <Titulo>Sign In</Titulo>
       <Seccion>
         <EmailIcon/>
-        <Input placeholder='E-mail' />
+        <Input
+          type="email"
+          placeholder='E-mail'
+          name='email'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
       </Seccion>
       <Seccion>
         <LockIcon/>
-        <Input placeholder='Password' />
+        <Input
+          type={showPassword ? "text" : "password"}
+          placeholder='Password'
+          name='password'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        {
+          showPassword === false ?
+          <VisibilityOffIcon onClick={() => {setShowPassword(!showPassword)} }/> : 
+          <VisibilityIcon onClick={() => {setShowPassword(!showPassword)} }/>
+        }
       </Seccion>
       <Boton>Log In</Boton>
       <Text2>Not a member?
--- clase17-mdt/src/pages/login/SignUp.jsx
+++ clase17-mdt/src/pages/login/SignUp.jsx
@@ -1,36 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import GoTo from '../../components/GoTo';
 import { Boton, Check, Formulario, Input, Seccion, Text1, Text2, Titulo } from '../../style/LoginStyle';
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useFormik } from 'formik';
+
 
 
 function SignUp({ setIsLogged }) {
+  const [showPassword, setShowPassword] = useState(false)
 
-    const navigate = useNavigate();
-    const handlerSubmit = (e) => {
-    e.preventDefault();
-    setIsLogged(true);
-    navigate("/home");
-  }
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: ""
+    },
+    onSubmit: (values) => {
+      setIsLogged(true);
+      navigate("/home");
+    }
+  })
 
   return (
     <>
-    <Formulario onSubmit={handlerSubmit}>
+    <Formulario onSubmit={formik.handleSubmit}>
       <Titulo>Sign Up</Titulo>
       <Seccion>
         <PersonIcon/>
-        <Input placeholder='Name'/>
+        <Input
+          type="text"
+          placeholder='Name'
+          name='name'
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
       </Seccion>
       <Seccion>
         <EmailIcon/>
-        <Input placeholder='E-mail'/>
+        <Input
+          type="email"
+          placeholder='E-mail'
+          name='email'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
       </Seccion>
       <Seccion>
         <LockIcon/>
-        <Input placeholder='Password'/>
+        <Input
+          type={showPassword ? "text" : "password"}
+          placeholder='Password'
+          name='password'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        {
+          showPassword === false ?
+          <VisibilityOffIcon onClick={() => {setShowPassword(!showPassword)} }/> : 
+          <VisibilityIcon onClick={() => {setShowPassword(!showPassword)} }/>
+        }
+        
       </Seccion>
       <Check type='checkbox'/> <Text1>I read and agree to Terms & Conditions</Text1>
       <Boton>create account</Boton>
